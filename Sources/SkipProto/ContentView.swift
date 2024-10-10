49901883af3be332f54182e70c2368dfc6bcf149
Sinan Karakaya <karakaya.sinan@proton.me>
2024-10-11 03:21:15 +0900

    Added marker on map

--- Sources/SkipProto/ContentView.swift
+++ Sources/SkipProto/ContentView.swift
@@ -35,13 +35,21 @@ public struct ContentView: View {
             NavigationStack {
                 #if !SKIP
                 if #available(iOS 17.0, macOS 14.0, *) {
-                    Map(initialPosition: .region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: latitude, longitude: longitude), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))))
+                    Map {
+                        Marker("Test", coordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude))
+                    }
+                    .mapStyle(.imagery(elevation: .realistic))
+                } else {
+                    Text("requires iOS 17.0 or macOS 14.0")
+                        .font(.title)
                 }
                 #else
                 ComposeView { ctx in
                     GoogleMap(cameraPositionState: rememberCameraPositionState {
                         position = CameraPosition.fromLatLngZoom(LatLng(latitude, longitude), Float(12.0))
-                    })
+                    }) {
+                        Marker(title: "Test", state: MarkerState(position: LatLng(latitude, longitude)))
+                    }
                 }
                 #endif
             }
